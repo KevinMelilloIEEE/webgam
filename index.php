5cb94ba20c489242ad0aca627224f83a435cdf76
KevinMelilloIEEE <31476865+KevinMelilloIEEE@users.noreply.github.com>
2024-05-16 13:00:45 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+  if(file_exists('login.php')) include 'login.php';
+  $SiteTitle = "Email Lookup Tools";
+  $PageTitle = "Main Menu";
+  include 'header.php';
+  include 'main.php';
+  include 'footer.php';
+?>
